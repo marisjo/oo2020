b2eb5c037e337aa17760f3ff81421582647d28fa
Maris <55129122+marisjo@users.noreply.github.com>
2020-02-18 09:12:47 +0200

    Update Main.java

--- w02_klass/Main.java
+++ w02_klass/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Character x=new Character("Varulfura", "druid", "night elf", "feral", 6000, "DPS", 800, 200);
         System.out.println(x);
 
-        Character y=new Character("Grimnoth", "fruid", "dranei", "restoration", 6000, "healer", 800, 200);
+        Character y=new Character("Grimnoth", "druid", "dranei", "restoration", 6000, "healer", 800, 200);
         Sysyem.ot.println(y);
         
     }
