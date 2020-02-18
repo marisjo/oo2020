757f087e8b288fd8f78af30fce37ebeaeaa9f4fb
Maris <55129122+marisjo@users.noreply.github.com>
2020-02-18 08:13:39 +0200

    Create Main.java

---
+++ w02_klass/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[]args){
+        Character x=new Character("Varulfura", "druid", "elf", "feral", 6000, "DPS");
+        System.out.println(x);
+        
+    }
+}
